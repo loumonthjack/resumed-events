c409b3154c1c05568b837359aa60118cd0f2a77f
Loumonth Jack <loumonth.jack.jr@gmail.com>
2023-10-31 15:12:07 -0500

    add-webhook-endpoint

--- src/servers/main.ts
+++ src/servers/main.ts
@@ -7,13 +7,14 @@ import rateLimit from 'express-rate-limit';
 import { FULL_SERVER_URL, checkEnvironmentVariables, isProd, setEnvironment } from '../constants';
 import { renderTemplate } from '../templates';
 import networkingRoute from '../endpoints/networking/init';
-import webhookServer from './webhook';
+import webhookServer, { stripe } from './webhook';
+import { handleCharge } from '../services/payment';
 
 const expressServer = async () => {
   const app = express();
   setEnvironment();
   checkEnvironmentVariables();
-  
+
   app.use(helmet({
     contentSecurityPolicy: false,
     crossOriginOpenerPolicy: false,
@@ -24,6 +25,27 @@ const expressServer = async () => {
     max: 100,
     message: "Too many requests from this IP, please try again later."
   }));
+  app.post('/webhook', express.raw({ type: 'application/json' }), async (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, STRIPE_WEBHOOK_KEY);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+    // Handle the event
+    switch (event.type) {
+      case 'checkout.session.completed':
+        await handleCharge(event);
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send(200)
+  });
   app.set("trust proxy", 1);
 
   app.engine('mustache', mustacheExpress());
@@ -49,7 +71,7 @@ const expressServer = async () => {
   app.get('/health-check', (req, res) => res.status(200).send('OK'));
 
   app.use(networkingRoute);
-  
+
   app.all('*', (req: any, res: any) => res.status(404).send(renderTemplate('error')));
 
   app.listen(process.env.PORT, () => console.log(`Server listening running ${FULL_SERVER_URL}`));
