2d61a66046592e339dd32159db3441cedd613775
j$ <55032731+60fov@users.noreply.github.com>
2023-11-22 10:26:35 -0600

    format file

--- src/server/main.ts
+++ src/server/main.ts
@@ -15,54 +15,54 @@ import { FULL_SERVER_URL, NODE_ENV, STRIPE_WEBHOOK_KEY } from "./constants";
 import auth from "./services/auth";
 
 export const stripe = new Stripe(STRIPE_WEBHOOK_KEY);
-  const app = express();
+const app = express();
 
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again later.",
+  })
+);
+
+// TODO explain
+app.set("trust proxy", 1);
+
+// TODO use helmet https/cors
+// if (NODE_ENV === "production") app.use(sslRedirect());
+if (NODE_ENV === "production") {
   app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000,
-      max: 100,
-      message: "Too many requests from this IP, please try again later.",
+    cors({
+      origin: (origin: any, callback: any) => {
+        console.log("origin", origin);
+        if (!origin || [FULL_SERVER_URL].includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true,
     })
   );
+}
 
-  // TODO explain
-  app.set("trust proxy", 1);
+// TODO explain
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.post("/webhook/sendgrid", auth.webhookHandler);
 
-  // TODO use helmet https/cors
-  // if (NODE_ENV === "production") app.use(sslRedirect());
-  if (NODE_ENV === "production") {
-    app.use(
-      cors({
-        origin: (origin: any, callback: any) => {
-          console.log("origin", origin);
-          if (!origin || [FULL_SERVER_URL].includes(origin)) {
-            callback(null, true);
-          } else {
-            callback(new Error("Not allowed by CORS"));
-          }
-        },
-        credentials: true,
-      })
-    );
-  }
+app.use(router);
 
-  // TODO explain
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.post("/webhook/sendgrid", auth.webhookHandler);
+// TODO error handler
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  console.log(error);
+  return res.redirect("/error");
+};
 
-  app.use(router);
+app.use(errorHandler);
 
+app.listen(env.PORT, () =>
+  console.log(`Server listening running ${FULL_SERVER_URL}`)
+);
 
-  // TODO error handler
-  const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
-    console.log(error);
-    return res.redirect("/error");
-  };
-
-  app.use(errorHandler);
-
-  app.listen(env.PORT, () =>
-    console.log(`Server listening running ${FULL_SERVER_URL}`)
-  );
 export const expressServer = app;
